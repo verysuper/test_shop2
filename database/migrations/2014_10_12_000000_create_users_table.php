3d03b9794cccac96c590d8247477e6b78256bc75
verysuper <verysuper@live.com>
2018-10-09 13:51:07 +0800

    migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -1,5 +1,5 @@
 <?php
-
+// 檔案位置：database/migrations/2014_10_12_000000_create_users_table.php
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
@@ -13,14 +13,25 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
+        // 建立資料表
         Schema::create('users', function (Blueprint $table) {
+            // 會員編號
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            // Email
+            $table->string('email', 150);
+            // 密碼
+            $table->string('password', 60);
+            // 帳號類型（type）:用於識別登入會員身份
+            //  - A（Admin）: 管理者
+            //  - G（General）: 一般會員
+            $table->string('type', 1)->default('G');  // 帳號狀態
+            // 暱稱
+            $table->string('nickname', 50);
+            // 時間戳記
             $table->timestamps();
+            
+            // 鍵值
+            $table->unique(['email'], 'user_email_uk');
         });
     }
 
@@ -31,6 +42,7 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
+        // 移除資料表
         Schema::dropIfExists('users');
     }
 }
